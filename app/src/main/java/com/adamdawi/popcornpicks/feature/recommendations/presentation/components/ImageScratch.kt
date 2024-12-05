4b86e6a1409001cfb4c39700f0abc5d0b432c29b
Adam Dawidziuk <adamdawidziuk@onet.pl>
2024-12-05 22:11:18 +0100

    NEW: improve image scratching with tap gesture

--- app/src/main/java/com/adamdawi/popcornpicks/feature/recommendations/presentation/components/ImageScratch.kt
+++ app/src/main/java/com/adamdawi/popcornpicks/feature/recommendations/presentation/components/ImageScratch.kt
@@ -3,6 +3,7 @@ package com.adamdawi.popcornpicks.feature.recommendations.presentation.component
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -19,11 +20,9 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -37,12 +36,13 @@ fun ImageScratch(
     modifier: Modifier = Modifier,
     overlayImage: ImageBitmap,
     baseImage: Painter,
-    scratchingThreshold : Float = 0.8f
+    scratchingThreshold : Float = 0.8f,
+    scratchLineWidth : Dp = 32.dp,
+    scratchLineCap : StrokeCap = StrokeCap.Round
 ) {
-    val lines = remember {
+    val scratchLines = remember {
         mutableStateListOf<Line>()
     }
-    val density = LocalDensity.current
     val totalScratchedArea = remember {
         mutableFloatStateOf(0f)
     }
@@ -60,39 +60,57 @@ fun ImageScratch(
             modifier = Modifier
                 .graphicsLayer {
                     compositingStrategy = CompositingStrategy.Offscreen
-                },
+                }
         ) {
             Canvas(
                 modifier = Modifier
                     .fillMaxSize()
+                    .pointerInput(true) {
+                        detectTapGestures(
+                            onTap = { position ->
+                                val line = Line(
+                                    start = position,
+                                    end = position,
+                                    strokeWidth = scratchLineWidth.toPx()
+                                )
+                                //accumulate the total scratched area, including overlapping lines
+                                totalScratchedArea.floatValue += line.calculateArea()
+                                scratchLines.add(line)
+                            }
+                        )
+                    }
                     .pointerInput(true) {
                         detectDragGestures { change, dragAmount ->
                             change.consume()
 
                             val line = Line(
                                 start = change.position - dragAmount,
-                                end = change.position
+                                end = change.position,
+                                strokeWidth = scratchLineWidth.toPx()
                             )
+                            //accumulate the total scratched area, including overlapping lines
+                            totalScratchedArea.floatValue += line.calculateArea()
 
-                            lines.add(line)
-                            totalScratchedArea.floatValue = calculateTotalArea(lines.toList(), density)
+                            scratchLines.add(line)
                         }
                     }
             ) {
                 val imageSize = IntSize(width = size.width.toInt(), height = size.height.toInt())
                 val maxCanvasArea = this.size.width.toFloat() * this.size.height.toFloat()
 
-                //if total scratched area is less than scratching threshold then show overlay image
+                //if total scratched area is below the threshold, show the overlay image
                 if(totalScratchedArea.floatValue/maxCanvasArea < scratchingThreshold) {
-                    drawImage(overlayImage, dstSize = imageSize)
+                    drawImage(image = overlayImage, dstSize = imageSize)
                 }
-                lines.forEach { line ->
+
+                //draw the scratch lines with transparency to "erase" the overlay
+                scratchLines.forEach { line ->
                     drawLine(
-                        color = line.color,
+                        color = Color.Transparent,
                         start = line.start,
                         end = line.end,
-                        strokeWidth = line.strokeWidth.toPx(),
-                        cap = StrokeCap.Round,
+                        strokeWidth = line.strokeWidth,
+                        cap = scratchLineCap,
                         blendMode = BlendMode.Clear
                     )
                 }
@@ -101,32 +119,25 @@ fun ImageScratch(
     }
 }
 
-data class Line(
+private data class Line(
     val start: Offset,
     val end: Offset,
-    val color: Color = Color.Transparent,
-    val strokeWidth: Dp = 40.dp
-)
-
-fun calculateTotalArea(lines: List<Line>, density: Density): Float {
-    var sum = 0f
-    lines.forEach { line ->
-        val length = distance(line.start, line.end)
-        val lineWidth = with(density) { line.strokeWidth.toPx() }
-        sum += length * lineWidth
+    val strokeWidth: Float
+){
+    fun calculateArea(): Float {
+        val lineLength = calculateLength()
+        return lineLength * strokeWidth
     }
-    return sum
-}
-
-fun distance(start: Offset, end: Offset): Float {
-    return sqrt((end.x - start.x).pow(2) + (end.y - start.y).pow(2))
+    private fun calculateLength(): Float {
+        return sqrt((end.x - start.x).pow(2) + (end.y - start.y).pow(2))
+    }
 }
 
 @Composable
 @Preview
 fun ImageScratchPreview() {
     ImageScratch(
-        overlayImage = ImageBitmap.imageResource(R.drawable.overlay),
+        overlayImage = ImageBitmap.imageResource(R.drawable.popcorn_overlay3),
         baseImage = painterResource(R.drawable.poster)
     )
 }
