b590643cb04f14802971cd45daec056da7c899e2
Adam Dawidziuk <adamdawidziuk@onet.pl>
2025-03-29 16:26:57 +0100

    FIX: test application modules

--- app/src/androidTest/java/com/adamdawi/popcornpicks/core/TestApplication.kt
+++ app/src/androidTest/java/com/adamdawi/popcornpicks/core/TestApplication.kt
@@ -1,13 +1,9 @@
 package com.adamdawi.popcornpicks.core
 
 import android.app.Application
-import org.koin.core.context.GlobalContext.startKoin
 
 class TestApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            modules()
-        }
     }
 }
